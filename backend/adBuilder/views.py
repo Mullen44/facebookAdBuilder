ff8b02079d8584b03457f8b259abb1b7184dc9e9
Andrew Mullen <mullenandrew95@gmail.com>
2022-05-08 12:44:00 -0700

    Create AdBuilderView in views.py and define url routes for API

--- backend/adBuilder/views.py
+++ backend/adBuilder/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .serializers import AdBuilderSerializer
+from rest_framework import viewsets
+from .models import AdBuilder
 
 # Create your views here.
+
+class AdBuilderView(viewsets.ModelViewSet):
+    serializer_class = AdBuilderSerializer
+    queryset = AdBuilder.objects.all()
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -14,8 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+from adBuilder import views
+
+router = routers.DefaultRouter()
+router.register(r'adBuilders', views.AdBuilderView, 'adBuilder')
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls))
 ]
