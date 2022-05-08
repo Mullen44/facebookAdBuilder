57804d5f0923deed49e10468093604f594e3e879
Andrew Mullen <mullenandrew95@gmail.com>
2022-05-08 12:01:35 -0700

    Register AdBuilder Application in Installed_Apps section of backend/settings.py. Created data model AdBuilder with headline and image properties. Create an admin.

--- backend/adBuilder/admin.py
+++ backend/adBuilder/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import AdBuilder
 
 # Register your models here.
+
+class AdBuilderAdmin(admin.ModelAdmin):
+    list = ('headline', 'image')
+
+
+admin.site.register(AdBuilder, AdBuilderAdmin)
--- backend/adBuilder/models.py
+++ backend/adBuilder/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class AdBuilder(models.Model):
+    headline = models.CharField(max_length=200)
+    image = models.ImageField
+
+    def _str_(self):
+        return self.headline
