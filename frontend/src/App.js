d01d4835f6898eaa887f12cf057277da1f83fdd1
Andrew Mullen <mullenandrew95@gmail.com>
2022-05-09 16:46:35 -0700

    Add ImageURLs array for hardcoded images to be displayed. Create a figure that displays an image with a caption. Add two buttons at the end for saving to the database, and a cancel to clear the selections.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,53 @@
 import './App.css';
-import { Form } from 'semantic-ui-react'
-//import Image from 'react-bootstrap/Image'
-// import { Image } from
+
+import { Form } from "react-bootstrap"
+
+const imageURL = [
+    "https://image.shutterstock.com/image-vector/vector-design-elements-your-company-600w-709133980.jpg",
+    "https://image.shutterstock.com/image-photo/11-april-2018-white-tesla-600w-1074713426.jpg",
+    "https://image.shutterstock.com/image-photo/hong-kong-may-20-2019-600w-1417919888.jpg",
+    "https://image.shutterstock.com/image-photo/hong-kong-china-circa-january-600w-1643565031.jpg"
+]
+
 
 function App() {
     return(
         <div className="wrapper">
             <h1>Facebook Ad Builder</h1>
-            <form>
-                <label>
-                    <p>Input Headline</p>
-                    <input name = "Input Headline" />
-                </label>
-                <label>
-                    <p>Select Image</p>
-                    <select>
-                        <option value = "Image 1">Image 1</option>
-                        <option value = "Image 2">Image 2</option>
-                    </select>
-                </label>
-            </form>
+            <Form>
+                <Form.Group>
+                    <Form.Label>Headline</Form.Label>
+                    <Form.Control type="input" placeholder="Enter Ad Headline"/>
+                </Form.Group>
+                <h2></h2>
+                <Form.Label>Select Image</Form.Label>
+                <Form.Select aria-label="Image Select">
+                    <option value = "Image 1">Image 1</option>
+                    <option value = "Image 2">Image 2</option>
+                    <option value = "Image 3">Image 3</option>
+                    <option value = "Image 4">Image 4</option>
+                </Form.Select>
+
+            </Form>
+
+            <div>
+                <div>
+                    <h3>Ad Preview</h3>
+                    <figure className='figure' style={{ maxWidth: '22rem' }}>
+                        <img
+                            src={ imageURL[2]}
+                            className='Ad Image'
+                            alt='...'
+                        />
+                        <caption className='figure-caption'>Advertisement Headline.</caption>
+                    </figure>
+                </div>
+            </div>
+            <div>
+                <button>Save</button>
+                <button>Cancel</button>
+            </div>
+
 
         </div>
     )
