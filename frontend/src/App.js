db80753197a87461dd5f92829f956821bac75dbe
Andrew Mullen <mullenandrew95@gmail.com>
2022-05-09 15:08:53 -0700

    Create a form element for the front end that contains a text input field where users will input their headline and a selector where users will select what image they want to load into their add

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import { Form } from 'semantic-ui-react'
+//import Image from 'react-bootstrap/Image'
+// import { Image } from
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return(
+        <div className="wrapper">
+            <h1>Facebook Ad Builder</h1>
+            <form>
+                <label>
+                    <p>Input Headline</p>
+                    <input name = "Input Headline" />
+                </label>
+                <label>
+                    <p>Select Image</p>
+                    <select>
+                        <option value = "Image 1">Image 1</option>
+                        <option value = "Image 2">Image 2</option>
+                    </select>
+                </label>
+            </form>
+
+        </div>
+    )
 }
 
 export default App;
+
+
+    // https://image.shutterstock.com/image-vector/vector-design-elements-your-company-600w-709133980.jpg
