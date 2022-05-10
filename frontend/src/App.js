cc2d6275bf5d53913c4ebb6e79e1649d6746e92e
Andrew Mullen <mullenandrew95@gmail.com>
2022-05-09 18:38:31 -0700

    Change App.js from being a function to a class. Allowed for a constructor to be defined with properties headling: String and img: Int.  Allowed for dynamic control of the AdPreview. Implemented handleTextChange and handleImgChange methods to handle the updating of ad preview headling and image.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,59 +1,90 @@
 import './App.css';
+import React, { Component } from 'react';
 
 import { Form } from "react-bootstrap"
 
 const imageURL = [
-    "https://image.shutterstock.com/image-vector/vector-design-elements-your-company-600w-709133980.jpg",
+    "https://sites.imsa.edu/acronym/files/2019/03/advertising-777x437.png",
+    "https://www.stretchingabuckblog.com/wp-content/uploads/2013/11/home-depot-black-friday-ad-2013.jpg",
     "https://image.shutterstock.com/image-photo/11-april-2018-white-tesla-600w-1074713426.jpg",
     "https://image.shutterstock.com/image-photo/hong-kong-may-20-2019-600w-1417919888.jpg",
     "https://image.shutterstock.com/image-photo/hong-kong-china-circa-january-600w-1643565031.jpg"
 ]
 
 
-function App() {
-    return(
-        <div className="wrapper">
-            <h1>Facebook Ad Builder</h1>
-            <Form>
-                <Form.Group>
-                    <Form.Label>Headline</Form.Label>
-                    <Form.Control type="input" placeholder="Enter Ad Headline"/>
-                </Form.Group>
-                <h2></h2>
-                <Form.Label>Select Image</Form.Label>
-                <Form.Select aria-label="Image Select">
-                    <option value = "Image 1">Image 1</option>
-                    <option value = "Image 2">Image 2</option>
-                    <option value = "Image 3">Image 3</option>
-                    <option value = "Image 4">Image 4</option>
-                </Form.Select>
+class App extends React.Component {
 
-            </Form>
+    constructor(props) {
+        super(props)
+        this.state = {
+            headline: '',
+            img: 0
+        }
 
-            <div>
+        this.handleImgChange = this.handleImgChange.bind(this)
+
+        this.handleTextChange = this.handleTextChange.bind(this)
+    }
+
+    handleTextChange(event) {
+        this.setState({headline: event.target.value})
+    }
+
+    handleImgChange(event) {
+        this.setState({img: event.target.value})
+    }
+
+    
+    render() {
+        return (
+            <div className="wrapper">
+                <h1>Facebook Ad Builder</h1>
+                <form>
+                    <Form.Group>
+                        <Form.Label>Headline: </Form.Label>
+                        <input type="text" value={this.state.headline} onChange={this.handleTextChange}/>
+                    </Form.Group>
+                    <h2></h2>
+                    <Form.Label>Select Image: </Form.Label>
+                    <select  value={this.state.img} onChange={this.handleImgChange}>
+                        <option value={1}>Image 1</option>
+                        <option value={2}>Image 2</option>
+                        <option value={3}>Image 3</option>
+                        <option value={4}>Image 4</option>
+                    </select>
+
+                </form>
+
                 <div>
-                    <h3>Ad Preview</h3>
-                    <figure className='figure' style={{ maxWidth: '22rem' }}>
-                        <img
-                            src={ imageURL[2]}
-                            className='Ad Image'
-                            alt='...'
-                        />
-                        <caption className='figure-caption'>Advertisement Headline.</caption>
-                    </figure>
+                    <div>
+                        <h3>Ad Preview</h3>
+                        <figure className='figure' style={{maxWidth: '22rem'}}>
+                            <img
+                                src={imageURL[this.state.img]}
+                                className='Ad Image'
+                                alt='...'
+                                width={300}
+
+                            />
+                            <div>
+                                <caption className='figure-caption'>
+                                    <span>{this.state.headline}</span>
+                                </caption>
+                            </div>
+                        </figure>
+                    </div>
                 </div>
-            </div>
-            <div>
-                <button>Save</button>
-                <button>Cancel</button>
-            </div>
+                <div>
+                    <button>Save</button>
+                    <button>Cancel</button>
+                </div>
 
 
-        </div>
-    )
+            </div>
+        )
+    }
 }
 
 export default App;
 
 
-    // https://image.shutterstock.com/image-vector/vector-design-elements-your-company-600w-709133980.jpg
